081d872e1a686062e0baa551a243dcf0e0f4125f
BrunoDutra98 <brunodutra46@gmail.com>
2017-05-10 18:47:31 -0300

    Modulo sessao

---
+++ sessao.py
@@ -0,0 +1,15 @@
+sessao = []
+def criar_sessao(cod_sessao,cod_filme,cod_sala,horario):
+    pass
+def recuperar_sessao(cod_sessao):
+    pass
+def verificar_lotacao(cod_sessao):
+    pass
+def listar_sessoes():
+    pass
+def remover_sessao(cod_sessao):
+    pass
+def remover_todos_ingressos():
+    pass
+def iniciar_ingressos():
+    pass
