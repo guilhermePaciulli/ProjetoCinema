3efa01c4e411e2e245739cd352540820b62dd0ab
LUCAS LANZANI <41623894@mackenzie.br>
2017-05-09 22:41:59 -0300

    interface filme ingresso

---
+++ logica/filme.py
@@ -0,0 +1,16 @@
+filme=[]
+
+def cadastrar_filme(cod_filme,titulo,duracao,classificao,diretor,distribuidora,status,gênero):
+    pass
+
+def buscar_film(cod_filme):
+    pass
+
+def listar_filmes():
+    pass
+
+def remover_todos_filmes():
+    pass
+
+def iniciar_filmes():
+    pass
---
+++ logica/ingresso.py
@@ -0,0 +1,25 @@
+ingresso =[]
+
+def venda_ingreso_meia(cod_sessao):
+    pass
+
+def venda_ingresso_inteira(cod_sessao):
+    pass
+
+def listar_ingresso_vendidos(cod_sessao):
+    pass
+
+def listar_ingressos():
+    pass
+
+def buscar_ingresso(cod_ingresso):
+    pass
+
+def remover_ingresso(cod_ingresso):
+    pass
+
+def remover_todos_ingresso(cod_ingresso):
+    pass
+
+def iniciar_ingressos():
+    pass
