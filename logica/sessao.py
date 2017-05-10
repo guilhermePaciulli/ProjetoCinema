8d107deb0f87249b1549f0b7a329f3bfc8113d45
BrunoDutra98 <brunodutra46@gmail.com>
2017-05-10 19:20:14 -0300

    blablabla

--- logica/sessao.py
+++ logica/sessao.py
@@ -1,12 +1,27 @@
-sessao = []
+import filme
+import sala
+
+sessoes = []
+cod_sessao = 0
+
+def gerar_codigo():
+    cod_sessao+=1
+    return cod_sessao
+
 def criar_sessao(cod_sessao,cod_filme,cod_sala,horario):
-    pass
+    ss = gerar_codigo()
+    f = filme.buscar_filme(cod_filme)
+    s = sala.buscar_sala(cod_sala)
+
+    sessao = [ss,f,s,horario]
+    sessoes.append(sessao)
+    
 def recuperar_sessao(cod_sessao):
     pass
 def verificar_lotacao(cod_sessao):
     pass
 def listar_sessoes():
-    pass
+    return sessoes
 def remover_sessao(cod_sessao):
     pass
 def remover_todos_ingressos():
