6cf1ca7c6014bdddab8b322aa5a5763e13e60a5c
BrunoDutra98 <brunodutra46@gmail.com>
2017-05-11 20:24:43 -0300

    menu ator final

--- gui/menu_ator.py
+++ gui/menu_ator.py
@@ -1,6 +1,6 @@
 from logica import ator
 
-def menu_adicionar():
+def menu_adicionar_ator():
     print("\nAdicionar ator:\n")
     cod_ator = int(input("Digite o código do ator:"))
     nome = input("Digite o nome do ator:"))
@@ -15,9 +15,58 @@ def _imprimir_ator(ator):
     print("Idade do ator:",ator[3])
     print()
 
-def buscar_ator():
-    cod_ator = input("")
+def menu_buscar_ator():
+    print("\nBuscar ator por código:\n")
+    cod_ator = int(input("Digite o código do ator"))
     ator = ator.buscar_ator(cod_ator)
-    _imprimir_ator(ator)
+    if (cod_ator == None):
+        print("Ator não encontrado!")
+    else:
+        _imprimir_ator(ator)
+        
+def menu_listar_atores():
+    print("\nListar Atores:\n")
+    atores = ator.buscar_atores()
+    for a in atores:
+        _imprimir_ator(a)
 
+def menu_remover_ator():
+    print("\nRemover Ator:\n")
+    cod_ator = int(input("Digite o código do ator:"))
+    a = ator.remover_ator(cod_ator)
+    if (a == False):
+        print("Ator não encontrado!")
+    else:
+        print("Ator Removido")
+
+def menu_remover_atores():
+    ator.remover_atores()
+    print("Todos os atores foram removidos do banco de dados!")
+
+def mostrar_menu():
+    run_ator = True
+    menu = ("\n----------------\n"+
+             "(1) Adicionar novo ator \n" +
+             "(2) Listar atores \n" +
+             "(3) Buscar ator por código \n" +
+             "(4) Remover ator \n" +
+             "(5) Remover todos os atores \n" +
+             "(0) Voltar\n"+
+            "----------------")
     
+    while(run_medico):
+        print (menu)
+        op = int(input("Digite sua escolha: "))
+
+        if (op == 1):
+            menu_adicionar_ator()
+        elif(op == 2):
+            menu_listar_atores()
+        elif(op == 3):       
+            menu_buscar_ator()
+        elif (op == 4):
+            menu_remover_ator()
+        elif (op == 5):
+            menu_remover_atores()
+        elif (op == 0):
+            run_medico = False 
