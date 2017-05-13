8ef379314d2dabe79e376b4e0344234ba969a448
BrunoDutra98 <brunodutra46@gmail.com>
2017-05-12 21:00:21 -0300

    menu sala

--- gui/menu_sala.py
+++ gui/menu_sala.py
@@ -7,11 +7,14 @@ def menu_adicionar_sala():
     sala.salas.append(cod_sala,lotacao)
 
 def menu_sala_ocupada():
-    print("\nTornar sala ocupada!\n")
+    print("\nTornar sala ocupada:\n")
     cod_sala = int(input("Digite o código da sala:"))
     sala.definir_status_ocupada(cod_sala)
 
-
+def menu_sala_livre():
+    print("\nTornar sala livre:\n")
+    cod_sala = int(input("Digite o código da sala:"))
+    sala.definir_status_livre(cod_sala)
 
 def _imprimir_sala(sala):
     print("Código da sala:", sala[0])
@@ -41,7 +44,41 @@ def menu_remover_sala():
     cod_sala = int(input("Digite o código da sala:"))
     s = sala.remover_sala(cod_sala)
     if (s == False):
-        print("Sala nã encontrada!")
+        print("Sala não encontrada!")
     else:
         print("Sala removida!")
-    
+
+def menu_remover_todas_salas():
+    sala.remover_todas_salas()
+    print("Todas as salas foram apagadas do banco de dados!")
+
+def menu():
+    while(True):
+        print("1 - Adicionar nova sala")
+        print("2 - Tornar sala ocupada")
+        print("3 - Tornar sala livre")
+        print("4 - Buscar sala")
+        print("5 - Listar salas")
+        print("6 - Remover sala por código")
+        print("7 - Remover todas as salas")
+        print("0 - Voltar")
+
+        op = int(input("Digite sua escolha: "))
+
+        if (op == 1):
+            menu_adicionar_sala()()
+        elif(op == 2):
+            menu_sala_ocupada()
+        elif(op == 3):
+            menu_sala_livre()
+        elif (op == 4):
+            menu_buscar_sala()
+        elif (op == 5):
+            menu_listar_salas()
+        elif (op == 6):
+            menu_remover_sala()
+        elif (op == 7):
+            menu_remover_todas_salas()
+        elif (op == 0):
+            break
+
