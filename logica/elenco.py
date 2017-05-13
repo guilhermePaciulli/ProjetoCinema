2f79d998d2f10ba3e0ba1a093efd9c5975cebbfb
guilhermePaciulli <gpaciulli100@gmail.com>
2017-05-12 22:38:13 -0300

    modificacao elenco

--- logica/elenco.py
+++ logica/elenco.py
@@ -5,16 +5,19 @@ elencos = []
 
 def adicionar_ator(cod_elenco, cod_ator, cod_filme, tipo):
     if(ator.buscar_ator(cod_ator) != None and filme.buscar_filme(cod_filme) != None):
+        e = buscar_elenco(cod_elenco)
         a = ator.buscar_ator(cod_ator)
-        f = filme.buscar_filme(cod_filme)
-        elencos.append([cod_elenco, a, f, tipo])
+        if e != None:
+            elencos[elencos.index(e)][1].append(a)
+        else:
+            a = ator.buscar_ator(cod_ator)
+            f = filme.buscar_filme(cod_filme)
+            elencos.append([cod_elenco, [{a, tipo}], f])
 
 def consultar_atores_por_filme(cod_elenco):
-    elenco = []
     for e in elencos:
         if e[0] == cod_elenco:
-            elenco.append(e[1])
-    return elenco
+            return e[1]
 
 def buscar_elenco(cod_elenco):
     for e in elencos:
@@ -26,11 +29,9 @@ def remover_elenco(cod_elenco):
     elencos.remove(buscar_elenco(cod_elenco))
 
 def buscar_elenco_por_filme(cod_filme):
-    elenco = []
     for e in elencos:
         if e[2][0] == cod_filme:
-            elenco.append(e)
-    return elenco
+            return e[1]
 
 def remover_todos_elencos():
     global elencos
