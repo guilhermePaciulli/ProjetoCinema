0fa7f01d9ae2cac5d0e8b043b9cea46b26a964bc
BrunoDutra98 <brunodutra46@gmail.com>
2017-05-14 20:19:39 -0300

    menu ingresso final

--- gui/menu_ingresso.py
+++ gui/menu_ingresso.py
@@ -1,6 +1,7 @@
 from logica import sessao
 from logica import filme
 from logica import ingresso
+import menu_inicial
 
 def menu_vender_ingresso_meia():
     print("\nVender ingresso meia:\n")
@@ -45,41 +46,60 @@ def menu_listar_ingressos():
     for i in ingressos:
         _imprimir_ingresso(i)
 
-def menu_remover_filme():
-    print("\nRemover Filme:\n")
-    cod_filme = int(input("Digite o código do filme:"))
-    f = filme.remover_filme(cod_filme)
-    if (f == False):
-        print("Filme não encontrado!")
-    else:
-        print("Filme Removido!")
+def menu_listar_ingressos_vendidos():
+    print("\nListar ingressos vendidos por sessão:\n")
+    cod_sessao = int(input("Digite a sessão desejada:"))
+    ingressos = ingresso.listar_ingressos_vendidos(cod_sessao)
+    for i in ingressos:
+        _imprimir_ingresso(i)
 
-def menu_remover_todos_filmes():
-    filme.remover_todos_filmes()
-    print("Todos os filmes foram removidos do banco de dados!")
+def menu_remover_ingresso():
+    print("\nRemover Ingresso:\n")
+    cod_ingresso = int(input("Digite o código do ingresso:"))
+    i = ingresso.remover_ingreso(cod_ingresso)
+    if == False:
+        print("Ingresso não encontrado!")
+    else:
+        print("Ingresso Removido!")
+
+def menu_remover_todos_ingressos():
+    ingresso.remover_todos_ingresso()
+    print("Todos os ingressos foram removidos do banco de dados!")
 
 def menu():
     while(True):
-        print("1 - Adicionar novo filme")
-        print("2 - Listar filmes")
-        print("3 - Buscar filme por código")
-        print("4 - Remover filme")
-        print("5 - Remover todos os filmes")
-        print("0 - Voltar")
+        print("1 - Vender ingresso meia")
+        print("2 - Vender ingresso inteira")
+        print("3 - Buscar ingresso por código")
+        print("4 - Listar ingressos")
+        print("5 - Listar ingressos vendidos por sessão")
+        print("6 - Remover ingresso")
+        print("7 - Remover todos os ingressos")
+        print("8 - Voltar")
 
         op = int(input("Digite sua escolha: "))
+                
+        while op < 1 or op > 8:
+            op = int(input("Entre com uma escolha válida: "))
 
         if(op == 1):
-            menu_adicionar_filme()
+            menu_vender_ingresso_meia()
         elif(op == 2):
-            menu_listar_filmes()
+            menu_vender_ingresso_inteira()
         elif(op == 3):
-            menu_buscar_filme()
+            menu_buscar_ingresso()
         elif(op == 4):
-            menu_remover_filme()
+            menu_listar_ingressos()
         elif(op == 5):
-            menu_remover_todos_filmes()                           
-        elif(op == 0):
+            menu_listar_ingressos_vendidos()
+        elif(op == 6):
+            menu_remover_ingresso()
+        elif(op == 7):
+            menu_remover_todos_ingressos
+        elif(op == 8):
             break
+        menu_inicial.limparTela()
+
+
 
 
