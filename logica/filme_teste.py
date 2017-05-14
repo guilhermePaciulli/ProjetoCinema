16ef46fc5f527288018b1618d1beae8a3037e8ad
guilhermePaciulli <gpaciulli100@gmail.com>
2017-05-14 18:24:50 -0300

    Teste filme

--- logica/filme_teste.py
+++ logica/filme_teste.py
@@ -9,11 +9,11 @@ class FilmeTeste(unittest.TestCase):
     def test_cadastrar_filme(self):
         filme.cadastrar_filme(0, "Jurassic World", 3.2, 0, "Steven Spielberg", "Universal", "Em cartaz", "Aventura")
         filme.cadastrar_filme(0, "Jurassic World", 3.2, 0, "Steven Spielberg", "Universal", "Em cartaz", "Aventura")
-        self.assertEquals(2, len(filme.buscar_filme()))
+        self.assertEquals(2, len(filme.listar_filmes()))
 
     def test_buscar_filme(self):
         filme.cadastrar_filme(0, "Jurassic World", 3.2, 0, "Steven Spielberg", "Universal", "Em cartaz", "Aventura")
-        self.assertNotNone(filme.buscar_filme(0))
+        self.assertIsNotNone(filme.buscar_filme(0))
 
     def test_listar_filmes(self):
         filme.cadastrar_filme(0, "Jurassic World", 3.2, 0, "Steven Spielberg", "Universal", "Em cartaz", "Aventura")
@@ -22,15 +22,17 @@ class FilmeTeste(unittest.TestCase):
         filme.cadastrar_filme(3, "Jurassic World", 3.2, 0, "Steven Spielberg", "Universal", "Em cartaz", "Aventura")
         filme.cadastrar_filme(4, "Jurassic World", 3.2, 0, "Steven Spielberg", "Universal", "Em cartaz", "Aventura")
         filme.cadastrar_filme(5, "Jurassic World", 3.2, 0, "Steven Spielberg", "Universal", "Em cartaz", "Aventura")
-        self.assertEquals(6, len(filme.buscar_filme()))
+        self.assertEquals(6, len(filme.listar_filmes()))
 
     def test_remover_todos_filmes(self):
         filme.cadastrar_filme(0, "Jurassic World", 3.2, 0, "Steven Spielberg", "Universal", "Em cartaz", "Aventura")
         filme.cadastrar_filme(1, "Jurassic World", 3.2, 0, "Steven Spielberg", "Universal", "Em cartaz", "Aventura")
         filme.remover_todos_filmes()
-        self.assertEquals(0, len(filme.buscar_filme()))
+        self.assertEquals(0, len(filme.listar_filmes()))
 
-    def test_iniciar_filmes():
+    def test_iniciar_filmes(self):
         filme.iniciar_filmes()
-        self.assertEquals(2, len(filme.buscar_filme()))
-        
+        self.assertEquals(2, len(filme.listar_filmes()))
+
+if __name__ == '__main__':
+    unittest.main()
