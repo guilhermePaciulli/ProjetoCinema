a8170bf6213a4d9f2c18607437855a7b11d1f5b2
BrunoDutra98 <brunodutra46@gmail.com>
2017-05-14 12:05:40 -0300

    menu filme final

--- gui/menu_filme.py
+++ gui/menu_filme.py
@@ -12,76 +12,66 @@ def menu_adicionar_filme():
     genero = input("Digite o genero do filme:"))
     filme.cadastrar_filme(cod_filme, titulo, duracao, classificao, diretor, distribuidora, status, genero)
 
-def _imprimir_elenco(elenco):
-    print("Código do elenco:",elenco[0])
-    atores = elenco.consultar_atores_por_filme(elenco[0])
-    for a in atores:
-        print("Ator do elenco:",a[1])
-        print("Nacionalidade do ator:",ator[2])
-        print("Tipo do ator:",ator[3])
-    print()
-
-def menu_buscar_elenco():
-    print("\nBuscar elenco por código:\n")
-    cod_elenco = int(input("Digite o código do elenco"))
-    elenco = elenco.buscar_elenco(cod_elenco)
-    if (cod_elenco == None):
-        print("Elenco não encontrado!")
-    else:
-        _imprimir_elenco(elenco)
-
-def menu_buscar_elenco_por_filme():
-    print("\nBuscar elenco por filme:\n")
+def _imprimir_filme(filme):
+    print("Código do filme:",filme[0])
+    print("Título do filme:",filme[1])
+    print("Duração do filme:",filme[2])
+    print("Classificação do filme:",filme[3])
+    print("Diretor do filme:",filme[4])
+    print("Distribuidor do filme:",filme[5])
+    print("Status do filme:",filme[6])
+    print("Gênero do filme:",filme[7])
+    print("")
+    
+def menu_buscar_filme():
+    print("\nBuscar filme por código:\n")
     cod_filme = int(input("Digite o código do filme:"))
-    elenco = elenco.buscar_elenco_por_filme(cod_filme)
+    filme = filme.buscar_filme(cod_filme)
     if (cod_filme == None):
-        print("Elenco não encontrado!")
+        print("Filme não encontrado!")
     else:
-        _imprimir_elenco(elenco)
+        _imprimir_filme(filme)
 
-def menu_listar_elencos():
-    print("\nListar elencos:\n")
-    elenco = elenco.buscar_elenco()
-    for e in elencos:
-        _imprimir_elenco(e)
+def menu_listar_filmes():
+    print("\nListar filmes:\n")
+    filmes = filme.listar_filmes()
+    for f in filmes:
+        _imprimir_filme(f)
 
-def menu_remover_elenco():
-    print("\nRemover Elenco:\n")
-    cod_elenco = int(input("Digite o código do elenco:"))
-    e = elenco.remover_elenco(cod_elenco)
-    if (e == False):
-        print("Elenco não encontrado!")
+def menu_remover_filme():
+    print("\nRemover Filme:\n")
+    cod_filme = int(input("Digite o código do filme:"))
+    f = filme.remover_filme(cod_filme)
+    if (f == False):
+        print("Filme não encontrado!")
     else:
-        print("Elenco Removido")
+        print("Filme Removido!")
 
-def menu_remover_todos_elencos():
-    elenco.remover_todos_elencos()
-    print("Todos os elencos foram removidos do banco de dados!")
+def menu_remover_todos_filmes():
+    filme.remover_todos_filmes()
+    print("Todos os filmes foram removidos do banco de dados!")
 
 def menu():
     while(True):
-        print("1 - Adicionar novo elenco")
-        print("2 - Listar elencos")
-        print("3 - Buscar elenco por código")
-        print("4 - Buscar elenco por filme")
-        print("5 - Remover elenco")
-        print("6 - Remover todos os elencos")
+        print("1 - Adicionar novo filme")
+        print("2 - Listar filmes")
+        print("3 - Buscar filme por código")
+        print("4 - Remover filme")
+        print("5 - Remover todos os filmes")
         print("0 - Voltar")
 
         op = int(input("Digite sua escolha: "))
 
         if(op == 1):
-            menu_adicionar_elenco()
+            menu_adicionar_filme()
         elif(op == 2):
-            menu_listar_elencos()
+            menu_listar_filmes()
         elif(op == 3):
-            menu_buscar_elenco()
+            menu_buscar_filme()
         elif(op == 4):
-            menu_buscar_elenco_por_filme()
+            menu_remover_filme()
         elif(op == 5):
-            menu_remover_elenco()
-        elif(op == 6):
-            menu_remover_todos_elencos()                            
+            menu_remover_todos_filmes()                           
         elif(op == 0):
             break
 
